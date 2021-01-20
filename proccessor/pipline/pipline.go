c7f7040c1d0c2e74fed252b6d5fb7f8fb24a6382
zhouzhe <zhouzhe@xin.com>
2021-01-20 11:00:00 +0800

    增加管道轮询

--- proccessor/pipline/pipline.go
+++ proccessor/pipline/pipline.go
@@ -1,11 +1,17 @@
 package pipline
 
+const (
+	SuccessUtil = 1
+)
+
 type Pipline struct {
 	sessionId string // 会话ID
 
 	Key int `json:"key"` // pip 的顺序
 
 	Data PipData `json:"data"` // 管道数据
+
+	IsSuccessUntil int `json:"is_success_until"`
 }
 
 func CreatePipline(key int, data PipData) *Pipline {
@@ -24,7 +30,20 @@ func (p *Pipline) SetSessionId(id string) *Pipline {
 	return p
 }
 
-func (p Pipline) Start() error {
+func (p Pipline) Start() (err error) {
+	if p.IsSuccessUntil == SuccessUtil {
+		for {
+			if err = p.dispatch(); err == nil {
+				break
+			}
+		}
+	} else {
+		err = p.dispatch()
+	}
+	return
+}
+
+func (p Pipline) dispatch() error {
 	for _, ac := range p.Data.Actions {
 		err := ac.WithSessionId(p.sessionId).Run()
 		if err != nil {
